9bb8b33ffc96da3d66171532a909446621eb2860
Dewight <120032970+nolimitwhite23@users.noreply.github.com>
2023-12-18 02:44:26 -0500

    Finshed Project

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
-const rock = document.querySelector(".rock").addEventListener("click", myFunction)
-const paper = document.querySelector(".paper").addEventListener("click", myFunction)
-const scissors = document.querySelector(".scissors").addEventListener("click", myFunction)
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+let result = 0
+let losses = 0
+let tie = 0
 
-function updateText() {
-    let playerText = document.querySelector(".player").innerHTML = "Rock"
-    let computerText = document.querySelector(".computer").innerHTML = "Paper"
 
-}
+rock.addEventListener("click", () => myFunction("Rock")); 
+paper.addEventListener("click", () => myFunction("Paper"));
+scissors.addEventListener("click", () => myFunction("Scissors"));
 
-function myFunction() {
-    updateText()
+function myFunction(choice) {
+    const computerChoice = ["Rock", "Paper", "Scissors"]
+    const randomIndex = Math.floor(Math.random() * computerChoice.length);
+    const randomChoice = computerChoice[randomIndex]
+
+    if (
+        (choice === "Rock" && randomChoice === "Scissors") ||
+        (choice === "Paper" && randomChoice === "Rock") ||
+        (choice === "Scissors" && randomChoice === "Paper")
+    ) {
+        result += 1;
+    } else if (
+    (randomChoice === "Rock" && choice === "Scissors") ||
+    (randomChoice === "Paper" && choice === "Rock") || 
+    (randomChoice === "Scissors" && choice === "Paper")
+    ) {
+        losses += 1;
+    } else {
+        if (randomChoice === choice);
+        tie += 1;
+    }
+
+    document.querySelector(".player").innerHTML = `Player Picked : ${choice}`
+    document.querySelector(".computer").innerHTML = `Computer Picked: ${randomChoice}`
+    document.querySelector(".Wins").innerHTML = `Wins: ${result}`
+    document.querySelector(".Losses").innerHTML = `Losses: ${losses}`
+    document.querySelector(".Tie").innerHTML = `Tie: ${tie}`
 }
